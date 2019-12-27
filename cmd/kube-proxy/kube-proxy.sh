82a4d162139373996a90ebc42301789ef2e9445f
nicktming <2279563278@qq.com>
2019-12-27 13:32:17 +0800

    add coredns

--- cmd/kube-proxy/kube-proxy.sh
+++ cmd/kube-proxy/kube-proxy.sh
@@ -5,7 +5,6 @@ k8spath=/root/go/src/k8s.io/kubernetes/cmd
 config=""
 config="$config --kubeconfig=/root/ssl_keys/node/kubelet_kubeconfig"
 config="$config --hostname-override=master"
-config="$config --master=https://172.31.71.181:6443"
 
 $k8spath/kube-proxy/kube-proxy $config
 
