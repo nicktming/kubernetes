408d9da778c0262c612ead635fa1af80a90995f7
nicktming <2279563278@qq.com>
2019-12-27 12:03:58 +0800

    deploy kube-proxy kubelet

--- cmd/kube-proxy/kube-proxy.sh
+++ cmd/kube-proxy/kube-proxy.sh
@@ -5,7 +5,7 @@ k8spath=/root/go/src/k8s.io/kubernetes/cmd
 config=""
 config="$config --kubeconfig=/root/ssl_keys/node/kubelet_kubeconfig"
 config="$config --hostname-override=master"
-config="$config --network-plugin=cni"
+config="$config --master=https://172.31.71.181:6443"
 
 $k8spath/kube-proxy/kube-proxy $config
 
--- cmd/kubelet/kubelet.sh
+++ cmd/kubelet/kubelet.sh
@@ -5,6 +5,9 @@ k8spath=/root/go/src/k8s.io/kubernetes/cmd
 config=""
 config="$config --kubeconfig=/root/ssl_keys/node/kubelet_kubeconfig"
 config="$config --hostname-override=master"
+config="$config --network-plugin=cni"
+config="$config --cluster-domain=cluster.local"
+config="$config --cluster-dns=169.169.0.10"
 
 $k8spath/kubelet/kubelet $config
 
