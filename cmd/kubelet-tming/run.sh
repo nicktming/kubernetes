3160d2e49eb9b465e73b61a1c58b317208dd7613
mingtingzhang <mingtingzhang@cn0214006300m.local>
2020-05-27 16:57:40 +0800

    add run.sh

---
+++ cmd/kubelet-tming/run.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+homepath=/root/mingtingzhang
+k8spath=$homepath/k8s
+
+config=""
+config="$config --kubeconfig=$homepath/ssl_keys/node/kubelet_kubeconfig"
+config="$config --network-plugin=cni"
+config="$config --cluster-domain=cluster.local"
+config="$config --cluster-dns=169.169.0.10"
+config="$config --runtime-cgroups=/systemd/system.slice --kubelet-cgroups=/systemd/system.slice"
+
+./kubelet-tming $config
