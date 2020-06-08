9fed522f2fb3102a8f763499e81856c346b816e9
mingtingzhang <mingtingzhang@cn0214006300m.local>
2020-06-08 17:38:28 +0800

    add helpers

--- pkg/kubelet-tming/cm/helpers_unsupported.go
+++ pkg/kubelet-tming/cm/helpers_unsupported.go
@@ -14,14 +14,14 @@ const (
 )
 
 // MilliCPUToQuota converts milliCPU and period to CFS quota values.
-func MilliCPUToQuota(milliCPU, period int64) int64 {
-	return 0
-}
+//func MilliCPUToQuota(milliCPU, period int64) int64 {
+//	return 0
+//}
 
 // MilliCPUToShares converts the milliCPU to CFS shares.
-func MilliCPUToShares(milliCPU int64) int64 {
-	return 0
-}
+//func MilliCPUToShares(milliCPU int64) int64 {
+//	return 0
+//}
 
 // ResourceConfigForPod takes the input pod and outputs the cgroup resource config.
 //func ResourceConfigForPod(pod *v1.Pod, enforceCPULimit bool, cpuPeriod uint64) *ResourceConfig {
@@ -33,9 +33,9 @@ func MilliCPUToShares(milliCPU int64) int64 {
 //	return nil, nil
 //}
 
-func getCgroupProcs(dir string) ([]int, error) {
-	return nil, nil
-}
+//func getCgroupProcs(dir string) ([]int, error) {
+//	return nil, nil
+//}
 
 // GetPodCgroupNameSuffix returns the last element of the pod CgroupName identifier
 //func GetPodCgroupNameSuffix(podUID types.UID) string {
