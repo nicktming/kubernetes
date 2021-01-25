3e1b1d46b3d15c63e691ad5bf4293d2eed9bd9da
nicktming <2279563278@qq.com>
2021-01-25 23:05:27 +0800

    add pleg info

--- pkg/kubelet-new/pleg/pleg.go
+++ pkg/kubelet-new/pleg/pleg.go
@@ -1,5 +1,25 @@
 package pleg
 
+import (
+	"k8s.io/apimachinery/pkg/types"
+)
+
+type PodLifeCycleEventType string
+
+const (
+	ContainerStarted PodLifeCycleEventType = "ContainerStarted"
+)
+
+type PodLifecycleEvent struct {
+	ID types.UID
+
+	Type PodLifeCycleEventType
+
+	// TODO Data
+}
+
+
 type PodLifecycleEventGenerator interface {
 	Start()
 }
+
