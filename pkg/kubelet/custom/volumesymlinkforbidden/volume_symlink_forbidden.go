f4810e227b646bb86768cf90646a6dcfaed8feba
mingtingzhang <mingtingzhang@sensetime.com>
2021-02-18 20:00:18 +0800

    add volume symlink forbidden

--- pkg/kubelet/custom/volumesymlinkforbidden/volume_symlink_forbidden.go
+++ pkg/kubelet/custom/volumesymlinkforbidden/volume_symlink_forbidden.go
@@ -7,10 +7,16 @@ import (
 	"k8s.io/klog"
 	"strings"
 	"k8s.io/kubernetes/pkg/kubelet/lifecycle"
+	"k8s.io/kubernetes/pkg/util/mount"
+	"k8s.io/api/core/v1"
+	"k8s.io/kubernetes/pkg/volume"
+	"k8s.io/kubernetes/pkg/volume/nfs"
 )
 
 type volumeAdmitHandler struct {
 	result lifecycle.PodAdmitResult
+	mounter mount.Interface
+	nfsPlugin volume.VolumePlugin
 }
 
 var _ lifecycle.PodAdmitHandler = &volumeAdmitHandler{}
@@ -18,33 +24,68 @@ var _ lifecycle.PodAdmitHandler = &volumeAdmitHandler{}
 // NewRuntimeAdmitHandler returns a sysctlRuntimeAdmitHandler which checks whether
 // the given runtime support sysctls.
 func NewVolumeAdmitHandler() (*volumeAdmitHandler, error) {
-	return &volumeAdmitHandler{}, nil
+	plugins := nfs.ProbeVolumePlugins(volume.VolumeConfig{})
+
+	return &volumeAdmitHandler{
+		nfsPlugin:  plugins[0],
+	}, nil
 }
 
 // Admit checks whether the runtime supports sysctls.
 func (w *volumeAdmitHandler) Admit(attrs *lifecycle.PodAdmitAttributes) lifecycle.PodAdmitResult {
 	pod := attrs.Pod
+	errList := w.checkVolumeSymlink(pod)
+
+	return lifecycle.PodAdmitResult{
+		Admit: len(errList) == 0,
+		Reason: "UnexpectedAdmissionError",
+		Message: strings.Join(errList, "\n"),
+	}
+}
+
+func (w *volumeAdmitHandler) checkVolumeSymlink(pod *v1.Pod) []error {
 	errList := make([]string, 0)
 	for _, vol := range pod.Spec.Volumes {
-		if vol.HostPath != nil {
-			err, symlink := volumeSymlink(vol.HostPath.Path)
+		spec := volume.Spec{
+			Volume: &vol,
+		}
+		if vol.VolumeSource.NFS != nil {
+			mounter, err := w.nfsPlugin.NewMounter(spec, pod, nil)
+			if err != nil {
+				return []error{err}
+			}
+			dir := "/tmp/0218"
+			if err := mounter.SetUpAt(dir, volume.MounterArgs{}); err != nil {
+				return []error{err}
+			}
+			err, symlink := isSymlink(vol.HostPath.Path)
 			if err != nil || symlink {
 				if err == nil {
-					err = fmt.Errorf("%v is a symlink, it is not allowed.", vol.HostPath.Path)
+					err = fmt.Errorf("%v is a symlink which is not allowed.", vol.HostPath.Path)
+				}
+				errList = append(errList, err.Error())
+			}
+			umounter, err := w.nfsPlugin.NewUnmounter(pod.Name, pod.UID)
+			if err != nil {
+				return []error{err}
+			}
+			if err := umounter.TearDownAt(dir); err != nil {
+				return []error{err}
+			}
+		} else if vol.VolumeSource.HostPath != nil {
+			err, symlink := isSymlink(vol.HostPath.Path)
+			if err != nil || symlink {
+				if err == nil {
+					err = fmt.Errorf("%v is a symlink which is not allowed.", vol.HostPath.Path)
 				}
 				errList = append(errList, err.Error())
 			}
 		}
 	}
-
-	return lifecycle.PodAdmitResult{
-		Admit: len(errList) == 0,
-		Reason: "this version is not allowed to mount symlink",
-		Message: strings.Join(errList, "\n"),
-	}
+	return errList
 }
 
-func volumeSymlink(path string) (error, bool) {
+func isSymlink(path string) (error, bool) {
 	fileInfo, err := os.Lstat(path)
 	if err != nil {
 		return err, false
